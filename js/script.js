57198d2fb075654185edfd0b7038473b3e01ef19
Yiiii <tang.yi@me.com>
2016-10-13 10:39:55 -0400

    1013 05

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ var spriteGeometry;
 var spriteMaterial;
 var spriteSphere;
 
-var spd=0, accX=0,accY=0;
+var posX=0,posY=0,spdX=2, spdY=2, accX=0,accY=0;
 //================side function=================
 var onOrientationChange = function(data){
 	console.log('NEW DEVICE ORIENTATION DATA!:');
@@ -70,12 +70,12 @@ var onDeviceMotion= function(data){
 	console.log(data);
 	latestMdata=data;
 	var mText = "MOTION DATA: <br />";
-	mText += "Acc X 4:" + data.accelerationIncludingGravity.x + " <br />";
+	mText += "Acc X 05:" + data.accelerationIncludingGravity.x + " <br />";
 	mText += "Acc Y : " + data.accelerationIncludingGravity.y + " <br />";
 	mText += "Acc Z : " + data.accelerationIncludingGravity.z + " <br />";
 	document.getElementById("mData").innerHTML = mText;
-	accX=data.accelerationIncludingGravity.x*10;
-accY=data.accelerationIncludingGravity.y*10;
+	accX=data.accelerationIncludingGravity.x;
+accY=data.accelerationIncludingGravity.y;
 }
 
 var Vec3 = function (x,y,z){
@@ -123,12 +123,17 @@ function init(){
 	var ambientLight = new THREE.AmbientLight(0x404040,2.4);
 	scene.add(ambientLight);
 
+	spdX+=accX;
+	spdY+=accY;
+	posX+=spdX;
+	posY+=spdY;
 	//init the sphere sprite, aha
 	spriteGeometry = new THREE.SphereGeometry( 30, 40, 40 );
 	spriteMaterial = new THREE.MeshLambertMaterial ( {color: 0xF95043} );
 	spriteSphere = new THREE.Mesh( spriteGeometry, spriteMaterial );
 	scene.add( spriteSphere );
-	spriteSphere.position.set(0+spd+accX,0+spd+accY,0);
+	spriteSphere.position.set(posX,posY);
+
 
 }
 //=================SETUP() OVER================
@@ -139,7 +144,7 @@ function animatedRender(){
 	requestAnimationFrame( animatedRender );
 
 //move the sprite
-	spriteSphere.position.set(0+spd+accX,0+spd+accY,0);
+	spriteSphere.position.set(posX,posY);
 
 //set camera
 camera.position.set(0,0,300);
