03f5d6e14571a66664c7f4e63c3b4dbec67793fc
Yiiii <tang.yi@me.com>
2016-10-13 12:57:44 -0400

    1013 1616

--- js/script.js
+++ js/script.js
@@ -74,8 +74,8 @@ var onDeviceMotion= function(data){
 	mText += "Acc Y : " + data.accelerationIncludingGravity.y + " <br />";
 	mText += "Acc Z : " + data.accelerationIncludingGravity.z + " <br />";
 	document.getElementById("mData").innerHTML = mText;
-	accX=data.accelerationIncludingGravity.x*10;
-accY=data.accelerationIncludingGravity.y*10;
+	accX=data.accelerationIncludingGravity.x*100;
+accY=data.accelerationIncludingGravity.y*100;
 }
 
 var Vec3 = function (x,y,z){
