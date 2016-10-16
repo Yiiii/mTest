e8590d87c31ea632c88e4505a124b5937432cb58
Yiiii <tang.yi@me.com>
2016-10-16 00:18:47 -0400

    1015 today last one

--- js/script.js
+++ js/script.js
@@ -140,7 +140,6 @@ function createSprite(){
 	spriteGeometry = new THREE.SphereGeometry( spriteSize, 40, 40 );
 	spriteMaterial = new THREE.MeshLambertMaterial ( {color: 0xF95043} );
 	spriteSphere = new THREE.Mesh( spriteGeometry, spriteMaterial );
-	spriteSphere.updateMatrix();
 	scene.add( spriteSphere );
 	spriteSphere.position.set(posX,posY,0);
 }
@@ -224,7 +223,7 @@ if(hit){
 	spriteSize+=1;
 }
 spriteSize+=1;
-
+spriteSphere.updateMatrix();
 
 
 //move the sprite
